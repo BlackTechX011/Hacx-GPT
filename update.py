2cf0547d2f18d7afe72687e3164a2070401d4175
BlackTechX <155609122+BlackTechX011@users.noreply.github.com>
2024-03-17 11:36:08 +0530

    UPDATE Hacx GPT V3

--- update.py
+++ update.py
@@ -1,18 +1,6 @@
 import os
 import requests
 
-print(f"""\033[0m
-
-__________.__                 __   ___________           .__    ____  ___
-\______   \  | _____    ____ |  | _\__    ___/___   ____ |  |__ \   \/  /
- |    |  _/  | \__  \ _/ ___\|  |/ / |    |_/ __ \_/ ___\|  |  \ \     / 
- |    |   \  |__/ __ \\  \___|    <  |    |\  ___/\  \___|   Y  \/     \ 
- |______  /____(____  /\___  >__|_ \ |____| \___  >\___  >___|  /___/\  \ 
-        \/          \/     \/     \/            \/     \/     \/      \_/
-                                                                                                                                                    
-
-    """)
-
 def fetch_latest_file(url, filename):
     response = requests.get(url)
     if response.status_code == 200:
@@ -23,13 +11,8 @@ def fetch_latest_file(url, filename):
         print(f"Failed to fetch the latest version of {filename}")
 
 if __name__ == "__main__":
-    # Update main.py
+ 
     main_url = "https://raw.githubusercontent.com/BlackTechX011/Hacx-GPT/main/main.py"
-    fetch_latest_file(main_url, "main.py")
+    fetch_latest_file(main_url, "main_updated.py")
 
-    # Update setup.py
-    setup_url = "https://raw.githubusercontent.com/BlackTechX011/Hacx-GPT/main/setup.py"
-    fetch_latest_file(setup_url, "setup.py")
-
-    # Exit after updating
-    exit()
+    os.replace("main_updated.py", "V3")
